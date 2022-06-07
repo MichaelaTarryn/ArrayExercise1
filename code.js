f4e356fd5b5ba77575af7a1786fcb494ffb65e67
Michaela <Michaela-13>
2022-06-07 10:06:34 +0200

    first

---
+++ code.js
@@ -0,0 +1,16 @@
+//arrays            0--------1---------2-----------3---------4-------5------6--------7--------8--------9---
+let bestFriends = ['AJ', 'Charles', 'Meagan', 'Michaela', 'Zaida','Luke','Astel','Tamara','Latiefa','Tashriq'];
+
+// Linking 
+ let ul = document.getElementById('bestFriends');
+
+ 
+ // display in cosole all
+ console.log(bestFriends);
+
+
+ //display middle names
+ console.log(bestFriends[bestFriends.length/2])
+ 
+ //display last element
+ console.log(bestFriends[bestFriends.length-1])
